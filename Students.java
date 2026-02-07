1c7a2708048cb19b09a94acfc42145da90bbe45e
cbhargavi724-design <cbhargavi724@gmail.com>
2026-02-07 15:27:27 +0530

    Add files via upload

---
+++ Students.java
@@ -0,0 +1,30 @@
+
+
+class Students{
+    public static void main(String[] args) {
+        int a=-10;
+        int b=8;
+        System.out.println("Arithmetic Operator");
+        System.out.println(a+b);
+        System.out.println("Logical Operator");
+        if(a>0&&a%2==0)
+        {
+            System.out.println("a is positive and even");
+        }
+        else
+            {
+            System.out.println("a is not positive and even");
+        }
+        System.out.println("Unary Operator");
+        System.out.println(++a);
+        System.out.println("Relational Operator");
+        if(a>0)
+        {
+            System.out.println("a is positive");
+        }
+        else
+        {
+            System.out.println("a is not positive");
+        }
+        }
+    }
