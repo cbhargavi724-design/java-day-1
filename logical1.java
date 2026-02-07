5ef6d5c43cfb0d6a86ab9c3fb138f934c3bd322f
cbhargavi724-design <cbhargavi724@gmail.com>
2026-02-07 14:21:28 +0530

    Add files via upload

---
+++ logical1.java
@@ -0,0 +1,13 @@
+class logical1{
+    public static void main(String[] args) {
+        int marks=45;
+        if(marks>=90&&marks<=100)
+        {
+            System.out.println("A grade");
+        }
+        else
+        {
+            System.out.println("fail");
+        }
+        }
+    }
