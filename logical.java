4ce69f5f2acb2612bd4ae3b8f5bd9983978d04e6
cbhargavi724-design <cbhargavi724@gmail.com>
2026-02-07 15:24:33 +0530

    Add files via upload

---
+++ logical.java
@@ -0,0 +1,9 @@
+class logical{
+    public static void main(String[] args) {
+        int a=8;
+        int b=4;
+        System.out.println(a&b);
+        System.out.println(a^b);
+        System.out.println(~a);
+    }
+} 
