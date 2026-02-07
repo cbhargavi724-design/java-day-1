a1faa383bf7149640e9938403682b2d8ea47e48f
cbhargavi724-design <cbhargavi724@gmail.com>
2026-02-07 15:26:55 +0530

    Add files via upload

---
+++ unary.java
@@ -0,0 +1,12 @@
+class unary{
+    public static void main(String[] args) {
+        int a=9;
+        int b=5;
+        System.out.println(a++);
+        System.out.println(a--);
+        System.out.println(++a);
+        System.out.println(--a);
+        System.out.println(-a);
+        System.out.println(+a);
+    }
+}
