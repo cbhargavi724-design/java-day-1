ec632cc4654d26a89ec5bfc3c9099a12a19e9c3f
cbhargavi724-design <cbhargavi724@gmail.com>
2026-02-07 15:26:34 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,12 @@
+class swap{
+    public static void main(String[] args) {
+        int a=5;
+        int b=3;
+         int temp=a;
+        a=b;
+        b=temp;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(temp);
+    }
+}
