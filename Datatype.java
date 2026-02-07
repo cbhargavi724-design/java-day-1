1485615717042575093699717221b56df9594a56
cbhargavi724-design <cbhargavi724@gmail.com>
2026-02-07 15:27:57 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,23 @@
+public class Datatype{
+    public static void main(String[] args)
+    {
+        byte b=122;
+        short a=1111;
+        int c=123456;
+        long l=123456789;
+        float f=10.3f;
+        double d=122.99;
+        char ch='a';
+        char cha='#';
+        System.out.println(b);
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(ch);
+        System.out.println(cha);
+} 
+}
+    
+
